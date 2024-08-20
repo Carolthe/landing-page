2ec67e280220cbddf92534bd115ebcffdabc0a39
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-20 13:42:45 +0100

    landing-page

--- src/components/Foteer.jsx
+++ src/components/Foteer.jsx
@@ -1,16 +1,24 @@
+import Facebook from '../img/Facebook.png'
+import Instagram from '../img/Instagram.png'
+import Twitter from '../img/Twitter.png'
+import Pinterest from '../img/Pinterest.png'
+
+
 export default function Foteer (){
     return(
-        <div className="h-80 w-full bg-greenclaro grid justify-center ">
-            <p className="mx-28 mt-16">Sannyside</p>
+        <div className=" bg-greenclaro grid justify-center ">
+            <p className="mx-28 mt-16 mb-10">Sannyside</p>
             <div className="flex gap-20 mb-10">
             <p>About</p>
             <p>Services</p>
             <p>Projects</p>
             </div>
-            <div className="flex justify-center gap-10 mb-10">
-            <img src="#" />
-            <img src="#" />
-            <img src="#" />
+            <div className="flex justify-center gap-10 mb-28 mt-10">
+            <img className='w-5' src={Facebook} />
+            <img src={Instagram} />
+            <img src={Twitter} />
+            <img src={Pinterest} />
+
             </div>
         </div>
     )
