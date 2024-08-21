b57b19fd2707fe71e6a8fa136248ca05987ffe4d
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-21 19:52:08 +0100

    landing-page

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,23 @@ function App() {
 
 <>
 <Header />
-<Desing teste={FundoOvo}  />
-<Information title="Transform your brand" paragraph="We are a full-service creative agency specializang in helping brands grow fast. Engage your clients through compelling visuals that do most of the markenting for you." />
+<div className='md:flex'>
+<Desing className='' teste={FundoOvo}  />
+<Information className="" title="Transform your brand" paragraph="We are a full-service creative agency specializang in helping brands grow fast. Engage your clients through compelling visuals that do most of the markenting for you." />
+</div>
+<div className='md:flex'>
 <Copo imgcopo={FundoCopo} />
 <Information title="Stand out to the right audience" paragraph="Using a collaborative formula of designers, researchers, photographers, videographes, and copywriters, we'll bulind and extend your brand in digital places." />
+</div>
+<div className='md:flex'>
 <ImgFundo imgfundo={Graphic} paragraph="Great desing makes you memorable. We deliver artwork that underscores you brand message and captures potential clients' attention." />
 <ImgFundo imgfundo={Photography} paragraph="Increase your credicility by getting the most stunning, high-quality photos thay improve your business image." />
+</div>
+<div className='md:flex'>
 <Comentarios perfil={Perfil1} title="CLIENT TESTIMONIALS" paragraph="We put our trust in Sunnyside and they dellivered, making sure our needs were met and seadlines were always hit." name="Emily R." profession="Marketing Director"  />
 <Comentarios perfil={Perfil2} paragraph="Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience." name="Thomas S." profession="Chief Operating Officer" />
 <Comentarios perfil={Perfil3} paragraph="Increble end result! Our sales increased over 400% when we worked with Sunnyside, Highly recommended!" name="Jennie F." profession="Business Owner" />
+</div>
 <Imgs />
 <Foteer />
 </> 
