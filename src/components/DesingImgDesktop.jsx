e2f260984a3ab62f01aee4d73b7ccd92ce156ce5
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-27 21:33:23 +0100

    landing-page

--- src/components/DesingImgDesktop.jsx
+++ src/components/DesingImgDesktop.jsx
@@ -2,15 +2,15 @@ export default function DesingImgDesktop (){
     return(  
     <div className="hidden md:flex mb-[20%]">
         <div className="h-[120%] w-[66.6%] bg-cover bg-center bg-[url('./img/CerejaDesktop.png')] content-center"> 
-        <div className="justify-items-center grid mt-[40%] mb-[0.4%] ">
+        <div className="justify-items-center grid mt-[50%] mb-[0.4%] ">
         <p className=" font-fraunces text-[180%] font-bold text-emerald-800 ">Graphic Desingn</p>
-        <p className=" text-center font-semibold font-barlow-600 text-emerald-900 text-[120%] mx-[5%] my-[5%] " >Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients' attention.</p>
+        <p className="mb-[15%] text-center font-semibold font-barlow-600 text-emerald-900 text-[120%] mx-[5%] my-[5%] " >Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients' attention.</p>
         </div>
         </div>
         <div className=" h-[120%] w-[66.6%] bg-cover bg-center bg-[url('./img/LaranjaDesktop.png')] content-center"> 
-        <div className="justify-items-center grid mt-[40%] mb-[0.4%]">
+        <div className="justify-items-center grid mt-[50%] mb-[0.4%]">
         <p className=" font-fraunces text-[180%] font-bold text-emerald-800 ">Photography</p>
-        <p className=" text-center font-semibold font-barlow-600 text-emerald-900 text-[120%] mx-[5%] my-[5%]" >Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+        <p className="mb-[15%] text-center font-semibold font-barlow-600 text-emerald-900 text-[120%] mx-[5%] my-[5%]" >Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
         </div>
         </div>
     </div>
--- src/components/HeaderDesktop.jsx
+++ src/components/HeaderDesktop.jsx
@@ -2,8 +2,19 @@ export default function HeaderDesktop (){
     return(
     <div  className="hidden md:block">
         <div className="h-screen bg-cover bg-center bg-[url('./img/HeaderFundo.png')]">
-    <h1 className="text-white text-[150%] font-bold font-fraunces p-5">sunnyside</h1>
-    <h2 className="mt-28 text-[270%]  text-center text-white font-fraunces font-bold ">WE ARE CREATIVES</h2>
+     <div className="flex justify-end justify-between ">
+        <div>
+    <h1 className="text-white text-[160%] font-bold font-fraunces p-8">sunnyside</h1>
+    </div>
+    <div className="flex justify-center items-center  gap-5 p-8 font-barlow text-white font-medium ">
+    <p className="place-items-center hover:bg-opacity-35 hover:bg-white hover:rounded-full w-24
+     h-9 flex justify-center">About</p>
+    <p className="place-items-center hover:bg-opacity-35 hover:bg-white hover:rounded-full w-24 h-9 flex justify-center">Services</p>
+    <p className="place-items-center hover:bg-opacity-35 hover:bg-white hover:rounded-full w-24 h-9 flex justify-center">Projects</p>
+    <p className="place-items-center hover:bg-opacity-35 hover:bg-white hover:rounded-full w-24 h-9 flex justify-center">Contact</p>
+    </div>
+    </div>
+    <h2 className="mt-[4%] text-[270%]  text-center text-white font-fraunces font-bold ">WE ARE CREATIVES</h2>
         </div>
     </div>
     )
