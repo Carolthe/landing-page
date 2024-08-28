e14166751142dc1aa9d88cc0f6701d9e7a7a336c
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-28 16:28:20 +0100

    landing-page

--- src/components/HeaderDesktop.jsx
+++ src/components/HeaderDesktop.jsx
@@ -1,8 +1,10 @@
+import DownArrow from '../img/DownArrow.png'
+
 export default function HeaderDesktop (){
     return(
     <div  className="hidden md:block">
         <div className="h-screen bg-cover bg-center bg-[url('./img/HeaderFundo.png')]">
-     <div className="flex justify-end justify-between ">
+     <div className="flex justify-between ">
         <div>
     <h1 className="text-white text-[160%] font-bold font-fraunces p-8">sunnyside</h1>
     </div>
@@ -14,8 +16,11 @@ export default function HeaderDesktop (){
     <p className="place-items-center hover:bg-opacity-35 hover:bg-white hover:rounded-full w-24 h-9 flex justify-center">Contact</p>
     </div>
     </div>
+    <div className="grid justify-items-center">
     <h2 className="mt-[4%] text-[270%]  text-center text-white font-fraunces font-bold ">WE ARE CREATIVES</h2>
-        </div>
+    <img className="mt-[10%] " src={DownArrow} />
+     </div>  
+     </div>
     </div>
     )
 }
--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import Sidebar from '../img/Sidebar.png'
+import SidebarMobile from '../img/SidebarMobile.png'
+import DownArrow from '../img/DownArrow.png'
 
 export default function HeaderMobile (){
     const [sidebar, setSidebar]= useState (false)
@@ -9,10 +10,12 @@ export default function HeaderMobile (){
         <div className="h-screen bg-cover bg-center bg-[url('./img/HeaderMobileFundo.png')]">
     <div className="flex justify-between p-5">
     <h1 className="text-white text-[150%] font-bold font-fraunces ">sunnyside</h1>
-    <img src={Sidebar} className="mr-[2%]" onClick={showSidebar} />
+    <img src={SidebarMobile} className="mr-[2%]" onClick={showSidebar} />
    </div> 
+  <div className="grid justify-items-center">
    <h2 className="mt-28 text-[270%]  text-center text-white font-fraunces font-bold ">WE ARE CREATIVES</h2>
-       </div>
+   <img className="mt-[40%] w-[40%]  " src={DownArrow} />
+      </div>  </div>
         </div>
     )
 }
