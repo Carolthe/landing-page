c51fca41adbf9a6817210f6360ae415d673fa190
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-29 16:28:56 +0100

    landing-page

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -16,13 +16,29 @@ export default function HeaderMobile (){
    
    
    <div className={` top-0 left-0 
-     ${sidebarOpen ? 'w-[220px]  h-[280px] bg-white ': 'w-0'}transition-width duration-300 `}>
+     ${sidebarOpen ? 'w-[220px] rounded-lg h-[340px] bg-white '
+     : 'w-0'}transition-width duration-300 `}>
     
     
 
     <button onClick={() => setSidebarOpen(!sidebarOpen)}>
-    <img src={SidebarMobile} className="mr-[2%]"/>
-     {sidebarOpen ? 'Close' : 'Open'}
+    <img src={SidebarMobile} className="w-6 mt-3 ml-2 mr-[2%]"/>
+     {sidebarOpen ?
+     <div className="grid my-5 mx-14  ">
+     <button className="hover:bg-yellow-200 bg-yellow-400 w-28 h-12 rounded-full my-2 text-white ">
+      About
+     </button>
+     <button className="hover:bg-yellow-200 w-28 h-12 rounded-full my-2 text-slate-400 ">
+      Services
+     </button>
+     <button className="hover:bg-yellow-200 w-28 h-12 rounded-full my-2 text-slate-400 ">
+      Projects
+     </button>
+     <button className="hover:bg-yellow-200 w-28 h-12 rounded-full my-2 text-slate-400 ">
+      Contact
+     </button>
+     </div>
+      : ''}
      </button>
     <div>
         
